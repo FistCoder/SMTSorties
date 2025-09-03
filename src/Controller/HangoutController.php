aff2d6e8e261408a8cc044e32b5c819e20c26d51
Soifiaouir <soifiaouir@gmail.com>
2025-09-03 14:27:12 +0200

    commit 03/09 14h30

--- src/Controller/HangoutController.php
+++ src/Controller/HangoutController.php
@@ -2,45 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\Hangout;
+use App\Entity\User;
+use App\Form\FilterHangoutType;
+use App\Repository\CampusRepository;
+use App\Repository\HangoutRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/hangouts', name: 'hangout_')]
 final class HangoutController extends AbstractController
 {
+
+
+    public function __construct(private readonly HangoutRepository $hangoutRepository, private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
     #[Route('/', name: 'list')]
-    public function listHangouts(): Response
+    public function listHangouts(Request $request, CampusRepository $campusRepository): Response
     {
+        //creation du form
+        $filterForm = $this->createForm(FilterHangoutType::class);
+        $filterForm->handleRequest($request);
+
+        $isOrganizer = $filterForm->get('isOrganizer')->getData();
+        $isRegistered = $filterForm->get('isRegistered')->getData();
+        $isNotRegistered = $filterForm->get('isNotRegistered')->getData();
+        $isPast = $filterForm->get('isPast')->getData();
+
+        $filters['isOrganizer'] = $isOrganizer;
+        $filters['isRegistered'] = $isRegistered;
+        $filters['isNotRegistered'] = $isNotRegistered;
+        $filters['isPast'] = $isPast;
+
+        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
+            $filters = $filterForm->getData();
+
+        } else {
+            $filters = [];
+        }
+
+
+        //Appel du repo et transmition de l'utilisateur connecter
+        $user = $this->getUser();
+
+        if (!$user) {
+            // Gère le cas utilisateur non connecté (redirige, exception, etc.)
+            throw $this->createAccessDeniedException('Vous devez être connecté');
+        }
+        $hangouts = $this->hangoutRepository->findFilteredEvent($user, $filters);
+
+
+        dump($filters, $hangouts);
+
+        return $this->render('hangouts/list.html.twig',
+            ['hangouts' => $hangouts,
+                'filterForm' => $filterForm]);
     }
 
-    #[Route('/add', name: 'add')]
-    public function addHangout(): Response
-    {
-    }
-
-    #[Route('/modify/{id}', name: 'modify', requirements: ['id'=>'\d+'])]
-    public function modifyHangout(int $id): Response
-    {
-    }
-
-    #[Route('/delete/{id}', name: 'delete', requirements: ['id'=>'\d+'])]
-    public function deleteHangout(int $id): Response
-    {
-    }
-
-    #[Route('/cancel/{id}', name: 'cancel', requirements: ['id'=>'\d+'])]
-    public function cancelHangout(int $id): Response
-    {
-    }
-
-    #[Route('/subscribe/{id}', name: 'subscribe', requirements: ['id'=>'\d+'])]
-    public function subscribeToHangout(): Response
-    {
-    }
-
-    #[Route('/unsubscribe/{id}', name: 'unsubscribe', requirements: ['id'=>'\d+'])]
-    public function unsubscribeFromHangout(): Response
-    {
-    }
+//    #[Route('/add', name: 'add')]
+//    public function addHangout(): Response
+//    {
+//    }
+//
+//    #[Route('/modify/{id}', name: 'modify', requirements: ['id'=>'\d+'])]
+//    public function modifyHangout(int $id): Response
+//    {
+//    }
+//
+//    #[Route('/delete/{id}', name: 'delete', requirements: ['id'=>'\d+'])]
+//    public function deleteHangout(int $id): Response
+//    {
+//    }
+//
+//    #[Route('/cancel/{id}', name: 'cancel', requirements: ['id'=>'\d+'])]
+//    public function cancelHangout(int $id): Response
+//    {
+//    }
+//
+//    #[Route('/subscribe/{id}', name: 'subscribe', requirements: ['id'=>'\d+'])]
+//    public function subscribeToHangout(): Response
+//    {
+//    }
+//
+//    #[Route('/unsubscribe/{id}', name: 'unsubscribe', requirements: ['id'=>'\d+'])]
+//    public function unsubscribeFromHangout(): Response
+//    {
+//    }
 }
--- src/Repository/HangoutRepository.php
+++ src/Repository/HangoutRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Hangout;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -15,6 +16,70 @@ class HangoutRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Hangout::class);
     }
+
+    //methode pour gerer les filters et injections des données utilisateurs et du formulaire
+    public function findFilteredEvent(?User $user, array $filters)
+    {
+        $qb = $this->createQueryBuilder('h');
+
+        if ($user) {
+            $qb->andWhere('h.organizer = :user')
+                ->setParameter('user', $user);
+        }
+
+        if (!empty($filters['campus'])) {
+            $qb->andWhere('h.campus = :campus')
+                ->setParameter('campus', $filters['campus']);
+        }
+
+        if (!empty($filters['name'])) {
+            $qb->andWhere('h.name LIKE :name')
+                ->setParameter('name', '%' . $filters['name'] . '%');
+        }
+
+        if (!empty($filters['start'])) {
+            $qb->andWhere('h.startingDateTime >= :start')
+                ->setParameter('start', $filters['start']);
+        }
+
+        if (!empty($filters['end'])) {
+            $qb->andWhere('h.startingDateTime <= :end')
+                ->setParameter('end', $filters['end']);
+        }
+
+        if (!empty($filters['state'])) {
+            $qb->andWhere('h.state = :state')
+                ->setParameter('state', $filters['state']);
+        }
+
+        if ($isOrganizer) {
+            $qb->andWhere('h.organizer = :user')
+                ->setParameter('user', $user);
+        }
+
+        if ($isRegistered) {
+            $qb->join('h.participants', 'p')
+                ->andWhere('p = :user')
+                ->setParameter('user', $user);
+        }
+
+        if ($isNotRegistered) {
+            $qb->leftJoin('h.participants', 'p')
+                ->andWhere('p != :user OR p IS NULL')
+                ->setParameter('user', $user);
+        }
+
+        if ($isPast) {
+            $qb->andWhere('h.startingDateTime < :today')
+                ->setParameter('today', new \DateTime());
+        }
+
+
+        return $qb->getQuery()->getResult();
+
+    }
+
+
 
     //    /**
     //     * @return Hangout[] Returns an array of Hangout objects
